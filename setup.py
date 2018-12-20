19daedf42022d718861a069a1bb5aba91887dcac
Geraldo Castro <victormatheuscastro@gmail.com>
2018-12-20 11:03:37 -0300

    Add long description

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import setuptools
 with open('requirements.txt') as f:
     reqs = f.read().splitlines()
 
+with open('README.md') as d:
+    long_description = d.read()
 
 setuptools.setup(
     name='rest_sms_getway',
@@ -13,11 +15,16 @@ setuptools.setup(
     packages=['rest_sms_getway'],
     install_requires=reqs,
     keywords='python api rest sms getway',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='LGPLv3',
     url='https://github.com/exageraldo/RestSMSGetway',
     classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Development Status :: 1 - Planning',
         'Topic :: Utilities',
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Operating System :: OS Independent'
     ],
 )
