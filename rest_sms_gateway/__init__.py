b00c972e117764556fe2169b495790e05be2457d
Geraldo Castro <victormatheuscastro@gmail.com>
2019-02-28 22:37:16 -0300

    Separet connector and default API class

--- rest_sms_gateway/__init__.py
+++ rest_sms_gateway/__init__.py
@@ -1,2 +1,4 @@
 from rest_sms_gateway.connector import SMSGatewayConnector
 from rest_sms_gateway.client import SMSGatewayClient
+from rest_sms_gateway.message import Message
+from rest_sms_gateway.conversation import Conversation
